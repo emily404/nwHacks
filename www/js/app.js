3cb95fd9aa6464141b57cc19e404720a01273176
emily404 <yimi_404@hotmail.com>
2015-03-14 11:11:18 -0700

    change url name

--- www/js/app.js
+++ www/js/app.js
@@ -53,7 +53,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       url: '/chats',
       views: {
         'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
+          templateUrl: 'templates/tab-restaurants.html',
           controller: 'MapController'
         }
       }
