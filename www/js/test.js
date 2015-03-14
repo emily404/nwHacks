c19e9ad756f0acfbc689efadbd4a7f41357af729
kelvinau <kelvinau4413@hotmail.com>
2015-03-14 10:32:05 -0700

    Added table for food data

--- www/js/test.js
+++ www/js/test.js
@@ -1,41 +1,44 @@
 var fbRef = new Firebase('https://flickering-fire-8922.firebaseio.com/');
 var foodRef = fbRef.child('food');
 function setValue(){
-//	usersRef.set({
-//		1:{
-//			name: 'Eggplant',
-//			type:'vegetable',
-//			price: '1.29/lb'
-//		},
-//		2:{
-//			name: 'Broccoli',
-//			type:'vegetable',
-//			price: '1.23/lb'
-//		},
-//		3:{
-//			name: 'Apple',
-//			type:'fruit',
-//			price: '1.45/lb'
-//		},	
-//		4:{
-//			name: 'Banana',
-//			type:'fruit',
-//			price: '1.00/lb'
-//		},
-//		5:{
-//			name: 'Orange',
-//			type:'fruit',
-//			price: '1.01/lb'
-//		}
-//	});
+	foodRef.set({
+		1:{
+			name: 'Broccoli',
+			type:'vegetable',
+			price: '$1.23/lb'
+		},
+		2:{
+			name: 'Eggplant',
+			type:'vegetable',
+			price: '$1.29/lb'
+		},
+		3:{
+			name: 'Apple',
+			type:'fruit',
+			price: '$1.45/lb'
+		},	
+		4:{
+			name: 'Banana',
+			type:'fruit',
+			price: '$1.01/lb'
+		},
+		5:{
+			name: 'Orange',
+			type:'fruit',
+			price: '$1.00/lb'
+		}
+	});
 }
-
 
 function retrieveSeasonalData(){
 	foodRef.on("value", function(snapshot) {
+		$("#fruits").html("");
+		$("#vegetables").html("");
 		snapshot.forEach(function(childSnapshot){
-			var key = childSnapshot.key();
-			alert(childSnapshot.val().type);
+			if (childSnapshot.val().type == "fruit")
+				$("#fruits").append("<tr><td>"+childSnapshot.val().name+"</td><td>"+childSnapshot.val().price+"</td></tr>");
+			else
+				$("#vegetables").append("<tr><td>"+childSnapshot.val().name+"</td><td>"+childSnapshot.val().price+"</td></tr>");
 		})		
 	});
 }
