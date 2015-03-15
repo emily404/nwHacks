d1753addd1ed7ce95669660a107514e0943f0b8b
kelvinau <kelvinau4413@hotmail.com>
2015-03-14 17:48:38 -0700

    Added graphs for food data

--- www/js/data.js
+++ www/js/data.js
@@ -8,27 +8,27 @@ function setValue(section){
 			0:{
 				name: 'Broccoli',
 				type:'vegetable',
-				price: '$1.23/lb'
+				price:[1.56,1.22,1.39,1.89]
 			},
 			1:{
 				name: 'Eggplant',
 				type:'vegetable',
-				price: '$1.29/lb'
+				price: [1.29,1.00,1.59,1.44]
 			},
 			2:{
 				name: 'Apple',
 				type:'fruit',
-				price: '$1.45/lb'
+				price: [0.44,0.33,0.31,0.59]
 			},	
 			3:{
 				name: 'Banana',
 				type:'fruit',
-				price: '$1.01/lb'
+				price: [0.59,0.44,0.99,0.78]
 			},
 			4:{
 				name: 'Orange',
 				type:'fruit',
-				price: '$1.00/lb'
+				price: [0.69,0.49,0.88,0.23]
 			}
 		});
 	}
@@ -36,19 +36,61 @@ function setValue(section){
 
 	}
 	else if (section == 3){
-		
+			
 	}
 }
 
 function retrieveSeasonalData(){
+	var fruits_set = {labels:["January", "April", "July","October"]};
+	var vegetables_set = {labels:["January", "April", "July","October"]};
+	
+	var color_setting = ["rgba(220,220,220,","rgba(151,187,205,","rgba(82,154,190,","rgba(13,121,175,"];
+	
 	foodRef.on("value", function(snapshot) {
-		$("#fruits").html("");
-		$("#vegetables").html("");
+		i = 0 ;
+		j= 0;
+		var fruits_datasets = [];
+		var vegetables_datasets = [];
 		snapshot.forEach(function(childSnapshot){
-			if (childSnapshot.val().type == "fruit")
-				$("#fruits").append("<tr><td>"+childSnapshot.val().name+"</td><td>"+childSnapshot.val().price+"</td></tr>");
-			else
-				$("#vegetables").append("<tr><td>"+childSnapshot.val().name+"</td><td>"+childSnapshot.val().price+"</td></tr>");
-		})		
+			if (childSnapshot.val().type == "fruit"){
+				fruits_datasets.push(
+					{
+						label:childSnapshot.val().name,
+						fillColor: color_setting[i] + "0.2)",
+						strokeColor:  color_setting[i] +"1)",
+						pointColor: color_setting[i] +"1)",
+			            pointStrokeColor: "#fff",
+			            pointHighlightFill: "#fff",
+			            pointHighlightStroke: color_setting[i] +"1)",
+			            data: childSnapshot.val().price
+					}
+				);
+				i++;
+			}
+			else{
+				vegetables_datasets.push(
+						{
+							label:childSnapshot.val().name,
+							fillColor: color_setting[j] + "0.2)",
+							strokeColor: color_setting[j] + "1)",
+							pointColor: color_setting[j] + "1)",
+				            pointStrokeColor: "#fff",
+				            pointHighlightFill: "#fff",
+				            pointHighlightStroke: color_setting[j] + "1)",
+				            data: childSnapshot.val().price
+						}
+					);
+				j++;
+			}
+		});	
+		fruits_set.datasets = fruits_datasets;
+		vegetables_set.datasets = vegetables_datasets;
+		
+		var ctx = $("#fruits").get(0).getContext("2d");
+		var myLineChart = new Chart(ctx).Line(fruits_set,{graphTitleFontSize : 14,graphTitle : "Price Trend of Fruits in Greater Vancouver in 2015",xAxisLabel : "Month",yAxisLabel : "CAD / lb", inGraphDataShow: true});
+		ctx = $("#vegetables").get(0).getContext("2d");
+		myLineChart = new Chart(ctx).Line(vegetables_set,{graphTitleFontSize : 14,graphTitle : "Price Trend of Vegetables in Greater Vancouver in 2015",xAxisLabel : "Month",yAxisLabel : "CAD / lb",inGraphDataShow: true});
+		
+		
 	});
 }
